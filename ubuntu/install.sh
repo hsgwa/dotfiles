c18bf27673205166c27dfa26feaa0ca34a648282
hsgwa <hasegawa.ats@gmail.com>
2018-09-11 08:13:09 +0900

    add: at install

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,3 +1,8 @@
 sudo ln -s /home/hasegawa/dotfiles/ubuntu/90_myrule.rules /etc/udev/rules.d/90-myrule.rules
 sudo ln -s /home/hasegawa/dotfiles/ubuntu/remap.sh /usr/local/bin/remap.sh
 sudo ln -s /home/hasegawa/dotfiles/ubuntu/remap_mouse.sh /usr/local/bin/remap_mouse.sh
+sudo apt-get install -y at
+sudo chmod 700 /usr/local/bin/remap.sh
+sudo chmod 700 /usr/local/bin/remap_mouse.sh
+
+
