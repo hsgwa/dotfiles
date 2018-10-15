9f948f0fa4a7e0ed1321229d6b27632ae43d0308
root <hasegawa@isp.co.jp>
2018-10-15 03:55:24 +0000

    add: symbolic link and fix mkdir option

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,5 @@
-mkdir -r ~/.config/nvim
+mkdir -p ~/.config/nvim
 cd ~/.config/nvim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
+ln -s ~/dotfiles/vim/init.vim ~/.config/nvim/
