22c05698b71a7f2848a0a84b0960fd0c6112c246
hasegawa <hasegawa.ats@gmail.com>
2019-06-02 13:32:09 +0900

    visual studio code のインストール

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -17,6 +17,24 @@ fi
 
 sudo apt install -y brightness-controller
 
+# Visual Studio Code のインストール
+if [ "$(grep -r apandada1 /etc/apt/*)" = "" ]; then
+	echo リポジトリ packages.microsoft.com/repos/vscode が見つかりません。リポジトリの追加をします。
+	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+	sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+	rm -f microsoft.gpg
+	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+	sudo apt-get update
+
+	sudo apt-add-repository ppa:apandada1/brightness-controller
+	sudo apt update
+
+else
+	echo リポジトリ packages.microsoft.com/repos/vscode が見つかりました。リポジトリの追加をスキップします。
+fi
+sudo apt-get install code
+
+
 sudo apt install -y ranger
 rm -rf ~/.config/ranger && ln -sf ~/dotfiles/ranger ~/.config/ranger
 
