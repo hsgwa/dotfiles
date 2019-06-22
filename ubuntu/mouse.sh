eb996ffde1e9c413cad77f598f1bbed4a98fabe9
hasegawa <hasegawa.ats@gmail.com>
2019-06-22 16:04:38 +0900

    マウスの移動速度を変更

--- ubuntu/mouse.sh
+++ ubuntu/mouse.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g'
-Kensington Expert Wireless TB`
-xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" 1.5
-xinput --set-prop "$MOUSE" "Device Accel Constant Deceleration" 1.5
+# Kensington Expert Wireless TB`
+xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" 2.0
+xinput --set-prop "$MOUSE" "Device Accel Constant Deceleration" 3.5
 
