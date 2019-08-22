d9b24d107b0f0ef68a24d15a26847d8b4497949c
hsgwa <hasegawa.ats@gmail.com>
2019-08-22 17:48:38 +0900

    add: Bluetooth mouse setting

--- ubuntu/mouse.sh
+++ ubuntu/mouse.sh
@@ -5,3 +5,6 @@ MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's
 xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" 2.0
 xinput --set-prop "$MOUSE" "Device Accel Constant Deceleration" 3.5
 
+MOUSE=`xinput --list --short| grep -i bsmbb23s | cut -f 1 | cut -d' ' -f 5- | sed 's/\s\+$//g'`
+xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" 2.0
+xinput --set-prop "$MOUSE" "Device Accel Constant Deceleration" 3.0
