0cc818fdf04b0fa1af5a5150f6f8a7c7f6b3761d
hsgwa <hasegawa.ats@gmail.com>
2019-10-06 12:07:03 +0900

    mod: display name from DP1 to DP-1

--- ubuntu/home_monitor_setting.sh
+++ ubuntu/home_monitor_setting.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-xrandr --output DP1 --auto
-xrandr --output DVI-I-1-1 --auto --right-of DP1
+xrandr --output DP-1 --auto
+xrandr --output DVI-I-1-1 --auto --right-of DP-1
 
-xrandr --output eDP1 --off
+xrandr --output eDP-1 --off
+sh ./reload_wallpaper.sh
