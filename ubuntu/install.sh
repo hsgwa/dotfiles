0b4eada9146403e38e4f13fb0d783e9685668f99
hsgwa <hasegawa.ats@gmail.com>
2019-11-19 10:32:30 +0900

    add: silversearche-ag

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -18,6 +18,7 @@ fi
 sudo apt install -y brightness-controller
 sudo apt install -y manpages-ja
 sudo apt install -y curl
+sudo apt install -y silversearcher-ag
 
 # Visual Studio Code のインストール
 if [ "$(grep -r vscode /etc/apt)" = "" ]; then
