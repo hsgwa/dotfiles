c2ce2bf619ae052f3093f59b2859cb1a5b1ccd12
hsgwa <hasegawa.ats@gmail.com>
2020-02-10 13:46:38 +0900

    fix: script自体がkillされるバグを修正

--- ubuntu/reload_xkeysnail.sh
+++ ubuntu/reload_xkeysnail.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ps aux | grep xkeysnail | grep -v grep | perl -pe 's/ +/ /g' | cut -d' ' -f 2 | xargs -I@ sudo kill @
+ps aux | grep xkeysnail | grep -v grep | grep -v $0 | perl -pe 's/ +/ /g' | cut -d' ' -f 2 | xargs -I@ sudo kill @
 systemctl --user restart xkeysnail
