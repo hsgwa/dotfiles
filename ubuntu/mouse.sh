096896d9fe086cb1281c962e3428bba5396abdaa
hsgwa <hasegawa.ats@gmail.com>
2020-02-15 22:29:54 +0900

    mod: mouse speed

--- ubuntu/mouse.sh
+++ ubuntu/mouse.sh
@@ -4,7 +4,7 @@ OS_VERSION=`cat /etc/os-release | grep -i version_id | egrep -o "[0-9]+.[0-9]+"`
 
 if [ $OS_VERSION = '18.04' ] ; then
 	MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Kensington # Kensington Expert Wireless TB`
-	xinput --set-prop "pointer:$MOUSE"  "libinput Accel Speed" -0.8
+	xinput --set-prop "pointer:$MOUSE"  "libinput Accel Speed" -1.5
 else
 	MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Kensington # Kensington Expert Wireless TB`
 	xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" 2.0
