3cd512ccc79f747cee2fbc2c2b4650b31e74052f
hsgwa <hasegawa.ats@gmail.com>
2020-03-22 00:06:07 +0900

    add: cmigemo

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -167,6 +167,7 @@ sudo apt install -y sxiv
 sudo apt install -y tig, nfs-common
 sudo apt install -y docker-compose
 sudo apt install -y texlive-base texlive-latex-base dvipng texlive-latex-extra
+sudo apt install -y cmigemo
 
 
 if [ $(crontab -l | grep mouse.sh | wc -l ) -eq 0 ]; then
