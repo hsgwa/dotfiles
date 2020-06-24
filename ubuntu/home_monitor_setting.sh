a185e38e7274626d570b3dcb9328b1770a28d4a8
hsgwa <hasegawa.ats@gmail.com>
2020-06-24 15:55:26 +0900

    add: dual hdmi setting

--- ubuntu/home_monitor_setting.sh
+++ ubuntu/home_monitor_setting.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 cd `dirname $0`
 
-xrandr --output DP-1 --auto
-xrandr --output DVI-I-1-1 --auto --right-of DP-1
+# xrandr --output DP-1 --auto
+# xrandr --output DVI-I-1-1 --auto --right-of DP-1
+xrandr --output HDMI-1 --auto
+xrandr --output DVI-I-1-1 --auto --right-of HDMI-1
 
 xrandr --output eDP-1 --off
 sh ./reload_wallpaper.sh
