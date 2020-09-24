48bb4f748ed6b3bb96d751f4bf871ff96017cfd1
hassay <hasegawa.ats@gmail.com>
2020-09-24 20:48:43 +0900

    support bluetooth mouse

--- ubuntu/mouse.sh
+++ ubuntu/mouse.sh
@@ -3,7 +3,8 @@
 OS_VERSION=`cat /etc/os-release | grep -i version_id | egrep -o "[0-9]+.[0-9]+"`
 
 if [ $OS_VERSION = '18.04' ] ; then
-	MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Kensington # Kensington Expert Wireless TB`
+	# MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Kensington # Kensington Expert Wireless TB`
+	MOUSE=`xinput --list --short|grep -i "Expert Wireless TB Mouse"| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Expert # Kensington Expert Wireless TB`
 	xinput --set-prop "pointer:$MOUSE"  "libinput Accel Speed" -1.0
 else
 	MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Kensington # Kensington Expert Wireless TB`
