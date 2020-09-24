aa04491c5b9f7b85da7a2549baae00e292b50dce
hsgwa <hasegawa.ats@gmail.com>
2020-09-25 01:02:55 +0900

    fix: dongle use

--- ubuntu/mouse.sh
+++ ubuntu/mouse.sh
@@ -3,8 +3,8 @@
 OS_VERSION=`cat /etc/os-release | grep -i version_id | egrep -o "[0-9]+.[0-9]+"`
 
 if [ $OS_VERSION = '18.04' ] ; then
+	MOUSE=`xinput --list --short|grep -i "Expert Wireless TB" | cut -f 1|cut -d" " -f 5- |sed 's/\s\+$//g' | grep -e ^Expert -e ^Kensington`
 	# MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Kensington # Kensington Expert Wireless TB`
-	MOUSE=`xinput --list --short|grep -i "Expert Wireless TB Mouse"| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Expert # Kensington Expert Wireless TB`
 	xinput --set-prop "pointer:$MOUSE"  "libinput Accel Speed" -1.0
 else
 	MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Kensington # Kensington Expert Wireless TB`
