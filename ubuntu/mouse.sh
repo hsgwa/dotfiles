c9160fc81259f318f79149539e442737967720b0
hassay <hasegawa.ats@gmail.com>
2020-12-05 08:40:14 +0900

    support ubuntu 20.04

--- ubuntu/mouse.sh
+++ ubuntu/mouse.sh
@@ -2,7 +2,10 @@
 
 OS_VERSION=`cat /etc/os-release | grep -i version_id | egrep -o "[0-9]+.[0-9]+"`
 
-if [ $OS_VERSION = '18.04' ] ; then
+if [ $OS_VERSION = '20.04' ] ; then
+	MOUSE=`xinput --list --short|grep -i "Expert Wireless TB" | cut -f 1|cut -d" " -f 5- |sed 's/\s\+$//g' | grep -e ^Expert -e ^Kensington`
+	xinput set-prop "pointer:$MOUSE"  "libinput Accel Speed" -1.0
+elif [ $OS_VERSION = '18.04' ] ; then
 	MOUSE=`xinput --list --short|grep -i "Expert Wireless TB" | cut -f 1|cut -d" " -f 5- |sed 's/\s\+$//g' | grep -e ^Expert -e ^Kensington`
 	# MOUSE=`xinput --list --short|grep -i Kensington| cut -f 1|cut -d" " -f 5-|sed 's/\s\+$//g' | grep ^Kensington # Kensington Expert Wireless TB`
 	xinput --set-prop "pointer:$MOUSE"  "libinput Accel Speed" -1.0
